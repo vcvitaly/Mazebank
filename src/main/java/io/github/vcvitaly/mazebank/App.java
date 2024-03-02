cbf9aa98d23364460293a9426e6a0ef916ca1fdf
Vitalii Chura <vcvitaly@yahoo.com>
2024-03-02 10:10:09 +0200

    add login design

--- src/main/java/io/github/vcvitaly/mazebank/App.java
+++ src/main/java/io/github/vcvitaly/mazebank/App.java
@@ -1,4 +1,16 @@
 package io.github.vcvitaly.mazebank;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
--- src/main/java/io/github/vcvitaly/mazebank/controller/LoginController.java
+++ src/main/java/io/github/vcvitaly/mazebank/controller/LoginController.java
@@ -1,4 +1,14 @@
 package io.github.vcvitaly.mazebank.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public Label payeeAddressLbl;
+    public TextField payeeAddressFld;
+    public Label passwordLbl;
+    public TextField passwordFld;
+    public Button loginBtn;
+    public Label errorLbl;
 }
