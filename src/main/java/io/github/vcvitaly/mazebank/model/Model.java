c289a454037375aaace922ecd2d2d4e28e32dd2a
Vitalii Chura <vcvitaly@yahoo.com>
2024-03-02 07:48:21 +0200

    create the project structure

---
+++ src/main/java/io/github/vcvitaly/mazebank/model/Model.java
@@ -0,0 +1,4 @@
+package io.github.vcvitaly.mazebank.model;
+
+public class Model {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module io.github.vcvitaly.mazebank {
     requires javafx.controls;
     requires javafx.fxml;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
 
     opens io.github.vcvitaly.mazebank to javafx.fxml;
     exports io.github.vcvitaly.mazebank;
+    exports io.github.vcvitaly.mazebank.controller;
+    exports io.github.vcvitaly.mazebank.controller.admin;
+    exports io.github.vcvitaly.mazebank.controller.client;
+    exports io.github.vcvitaly.mazebank.model;
+    exports io.github.vcvitaly.mazebank.view;
 }
