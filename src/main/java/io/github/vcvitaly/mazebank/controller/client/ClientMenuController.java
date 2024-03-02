502ab69323b080d309dfa92a8205368bf45dd35f
Vitalii Chura <vcvitaly@yahoo.com>
2024-03-02 15:05:43 +0200

    add client menu design

---
+++ src/main/java/io/github/vcvitaly/mazebank/controller/client/ClientMenuController.java
@@ -0,0 +1,21 @@
+package io.github.vcvitaly.mazebank.controller.client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button dashboardBtn;
+    public Button transactionBtn;
+    public Button accountsBtn;
+    public Button profileBtn;
+    public Button logoutBtn;
+    public Button reportBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
