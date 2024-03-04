83a80567a4a40ac203d273809048af0d3d333aa5
Vitalii Chura <vcvitaly@yahoo.com>
2024-03-04 15:57:01 +0200

    add admin menu design

---
+++ src/main/java/io/github/vcvitaly/mazebank/controller/admin/AdminMenuController.java
@@ -0,0 +1,19 @@
+package io.github.vcvitaly.mazebank.controller.admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+    public Button createClientBtn;
+    public Button clientsBtn;
+    public Button depositBtn;
+    public Button logoutBtn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
