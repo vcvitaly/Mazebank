ddba05d6405944677eb650322f0ed610d6093bef
VItalii Chura <vcvitaly@yahoo.com>
2024-03-06 18:31:33 +0200

    implement the transaction model

--- src/main/java/io/github/vcvitaly/mazebank/model/Transaction.java
+++ src/main/java/io/github/vcvitaly/mazebank/model/Transaction.java
@@ -1,4 +1,27 @@
 package io.github.vcvitaly.mazebank.model;
 
+import java.time.LocalDate;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import lombok.Getter;
+
+@Getter
 public class Transaction {
+    private final StringProperty sender;
+    private final StringProperty receiver;
+    private final DoubleProperty amount;
+    private final ObjectProperty<LocalDate> date;
+    private final StringProperty message;
+
+    public Transaction(String sender, String receiver, double amount, LocalDate date, String message) {
+        this.sender = new SimpleStringProperty(sender);
+        this.receiver = new SimpleStringProperty(receiver);
+        this.amount = new SimpleDoubleProperty(amount);
+        this.date = new SimpleObjectProperty<>(date);
+        this.message = new SimpleStringProperty(message);
+    }
 }
