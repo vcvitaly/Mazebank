a1bba8fc3fb90d4ce6e6c2a2add26815760e2e17
VItalii Chura <vcvitaly@yahoo.com>
2024-03-06 16:52:03 +0200

    add client cell design

--- src/main/java/io/github/vcvitaly/mazebank/controller/admin/ClientCellController.java
+++ src/main/java/io/github/vcvitaly/mazebank/controller/admin/ClientCellController.java
@@ -1,4 +1,22 @@
 package io.github.vcvitaly.mazebank.controller.admin;
 
-public class ClientCellController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ClientCellController implements Initializable {
+    public Label firstNameLbl;
+    public Label lastNameLbl;
+    public Label payeeAddressLbl;
+    public Label chAccountLbl;
+    public Label svAccountLbl;
+    public Label dateLbl;
+    public Button deleteBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
