d837438a1db8a63c5546052789c6b4c43ab4be37
VItalii Chura <vcvitaly@yahoo.com>
2024-03-07 19:00:50 +0200

    colorize transaction icons

--- src/main/java/io/github/vcvitaly/mazebank/controller/client/TransactionCellController.java
+++ src/main/java/io/github/vcvitaly/mazebank/controller/client/TransactionCellController.java
@@ -1,12 +1,14 @@
 package io.github.vcvitaly.mazebank.controller.client;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import io.github.vcvitaly.mazebank.model.Model;
 import io.github.vcvitaly.mazebank.model.Transaction;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.paint.Color;
 
 public class TransactionCellController implements Initializable {
     public FontAwesomeIconView inIcon;
@@ -32,5 +34,17 @@ public class TransactionCellController implements Initializable {
         senderLbl.textProperty().bind(transaction.getSender());
         receiverLbl.textProperty().bind(transaction.getReceiver());
         amountLbl.textProperty().bind(transaction.getAmount().asString());
+        updateTransactionIcons();
+    }
+
+    private void updateTransactionIcons() {
+        final String payeeAddress = Model.getInstance().getClient().getPayeeAddress().get();
+        if (transaction.getSender().get().equals(payeeAddress)) {
+            outIcon.setFill(Color.RED);
+            inIcon.setFill(Color.LIGHTGREY);
+        } else {
+            inIcon.setFill(Color.GREEN);
+            outIcon.setFill(Color.LIGHTGREY);
+        }
     }
 }
